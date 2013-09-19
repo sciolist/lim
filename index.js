94cfcd7e61e16e651a36d6784c55c1f06e0c0f12
Daniel Pihlstrom <sciolist.se@gmail.com>
2013-09-19 22:16:55 +0200

    wip

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-module.exports = require('traceur').require('./lib/index');
+var t = require('traceur').require('./lib/index.js');
+
+
+console.log(t.groupBy(0, 1, 2, 3) );
+console.log(t.groupBy(function(x) { return x % 2 }, [0,1,2,3,4,5,6]));
+
