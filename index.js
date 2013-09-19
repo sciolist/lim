84f016c29615f4c2d5602d04800edc222fbaf2d8
Daniel Pihlstrom <sciolist.se@gmail.com>
2013-09-19 22:40:29 +0200

    good old fashioned cleanup

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-var t = require('traceur').require('./lib/index.js');
-
-
-console.log(t.groupBy(0, 1, 2, 3) );
-console.log(t.groupBy(function(x) { return x % 2 }, [0,1,2,3,4,5,6]));
-
+var _ = require('traceur').require('./lib/index.js');
